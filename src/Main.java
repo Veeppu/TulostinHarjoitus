eab7123dbe2ce0552918caa4ef9dcfd4e9da2f6a
Veeppu <147065573+Veeppu@users.noreply.github.com>
2023-10-06 14:01:40 +0300

    Valmis!

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,34 @@ public class Main {
 		
 		String tekija = "Vera";
 		
+		double luku1 =6;
+		double luku2 =3;
+		
+		double tulo =0;
+		
 		System.out.println("Hei olen Tulostin-ohjelma");
 		System.out.println("Ohjelman tekijä: " + tekija);
 		
-		int luku1 =1;
-		int luku2 =3;
 		
 		//System.out.println(luku1);
 		
 		System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
-
+		System.out.println("Luku2-muuttujan arvo on " + luku2 + ".");
+		
+		
+		tulo = luku1 * luku2;
+		System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+		
+		tulo = luku1 - luku2;
+		System.out.println(luku1 + " - " + luku2 + " = " + tulo); 
+		
+		tulo = luku1 + luku2;
+		System.out.println(luku1 + " + " + luku2 + " = " + tulo); 
+		
+		tulo = luku1 / luku2;
+		System.out.println(luku1 + " / " + luku2 + " = " + tulo); 
+		
+		
 	}
 
 }
