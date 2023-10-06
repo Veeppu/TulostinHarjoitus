34a2c63b4db8791c1a91ced1444a9ea7167936af
Veeppu <147065573+Veeppu@users.noreply.github.com>
2023-10-06 13:46:15 +0300

    Puolessa välissä tehtävää

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
+		String tekija = "Vera";
+		
 		System.out.println("Hei olen Tulostin-ohjelma");
+		System.out.println("Ohjelman tekijä: " + tekija);
+		
+		int luku1 =1;
+		int luku2 =3;
+		
+		//System.out.println(luku1);
+		
+		System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
 
 	}
 
